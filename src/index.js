120fa8fb509bb629c564c9aac2a5444d75356700
Thammarith Likittheerameth <Thammarith.Likittheerameth@agoda.com>
2020-11-26 15:39:15 +0700

    Init

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/", (req, res) => {
+    res.send(req.body)
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
